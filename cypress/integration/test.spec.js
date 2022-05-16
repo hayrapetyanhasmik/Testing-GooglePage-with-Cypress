f1986218806a5d01274365b52b4ea06ad9d8ba9f
hayrapetyanhasmik <hasyokahasiko7@gmail.com>
2022-05-17 03:00:47 +0400

    Added a test checking URL

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,25 +1,25 @@
 /// <reference types = "cypress"/>
 describe("Testing smth on Google", function(){
     before(function(){
-        cy.visit('https://google.com');
+        cy.visit("https://google.com");
+        cy.url().should('include', 'google');
         cy.title().should('contain', 'Google');
-        
-        
     });
 
+    
     it("Search smth", function(){    
         cy.get('.gLFyf')
           .type('smth', {delay:200})
           .should('have.value', 'smth')
           .type('{selectall}{del}')
-          .type('anyth').clear().should('have.value', '')
+          .type('anyth').clear().should('have.value', '');
     });
     
     it("Toggle buttons", function(){
-        cy.get('input.gLFyf').type('cypress {enter}')
+        cy.get('input.gLFyf').type('cypress {enter}');
         cy.get('.ExCKkf').click()
           .get('.ly0Ckb').click()
           .get('.zgAlFc').click()
-          .get('.MUFPAc').contains('Картинки').click()
+          .get('.MUFPAc').contains('Картинки').click();
     });
 });
