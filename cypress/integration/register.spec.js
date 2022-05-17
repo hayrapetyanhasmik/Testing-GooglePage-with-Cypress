0a759ce882f55b836e5ef25fff80b0b794f8b42a
hayrapetyanhasmik <hasyokahasiko7@gmail.com>
2022-05-18 02:04:50 +0400

    Added a positive test

--- cypress/integration/register.spec.js
+++ cypress/integration/register.spec.js
@@ -1,13 +1,27 @@
 /// <reference types = "cypress"/>
-describe("Register on List.am", function(){
-    it('', function(){
+describe("Registration on List.am", function(){
+    beforeEach(function(){ 
         cy.visit('https://www.list.am/register');
+        cy.url().should('include', '/register');
+        cy.location('protocol').should('eq', 'https:')
+    })
+    it.skip('Check signup functionality with valid data', function(){
+        cy.get('#_idyour_email').type('hasyokahasiko7@gmail.com');
+        cy.get('#_idpassword').type('123456');
+        cy.get('#_idconfirm_password').type('123456');
+        cy.get('#_idverification_number').type(''); // ?
+        cy.get('#_idagree').click();
+        cy.get('#action__form_action0').click();
+        
+    });
+
+    it('Check signup functionality with invalid data', function(){
         cy.get('#_idyour_email').type('hasyokahasiko7@gmail.com');
         cy.get('#_idpassword').type('123456');
         cy.get('#_idconfirm_password').type('123457');
-        cy.get('#_idverification_number').type('44414');
+        cy.get('#_idverification_number').type('61087');
         cy.get('#_idagree').click();
-        cy.get('#registeraction__form_action0').click();
+        cy.get('#action__form_action0').click();
         cy.get('.error').should('contain', 'Մուտքագրված գաղնտաբառերը չեն համընկնում:Պատկերի վրա նշված թիվը սխալ է:')
     });
 });
